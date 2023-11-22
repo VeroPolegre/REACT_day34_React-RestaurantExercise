ddfd1d51552a173e34a490252f157df2eb8439b2
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-22 09:07:38 +0100

    text align

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Contact.css";
 const Contact = () => {
   return (
-    <div>
+    <div className="Contact">
       <h2>Contact Us</h2>
       <p>Feel free to reach out to us with any inquiries or reservations.</p>
     </div>
